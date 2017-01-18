ecb3926ffa22c76e32dee1ea0ac0638f52307afc
Jerry VanStrydonck <jvanstry@gmail.com>
2017-01-18 16:08:43 -0500

    added browerserify gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var nodemon = require('gulp-nodemon');
 var path = require('path');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
-gulp.task('default', ['browser-sync'], function () {
+gulp.task('default', ['browserify', 'browser-sync'], function () {
     gulp.watch(["public/styles.css", "public/game.html"], reload);
-    gulp.watch(["public/scripts/*.js"], reload);
+    gulp.watch(["public/scripts/*.js"], ['browserify', reload]);
 });
 
 gulp.task('browser-sync', ['nodemon'], function() {
@@ -30,4 +32,11 @@ gulp.task('nodemon', function (cb) {
       started = true; 
     } 
   });
+});
+
+gulp.task('browserify', function () {
+  return browserify('public/scripts/game-controller.js')
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./public/'));
 });
