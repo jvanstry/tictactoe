de365156e2a44a3af13bb1f846100cd710e4d3f9
Jerry VanStrydonck <jvanstry@gmail.com>
2017-01-26 01:43:45 -0500

    cpu specs

--- jasmine/spec/bundlespec.js
+++ jasmine/spec/bundlespec.js
@@ -5,19 +5,14 @@ var board = require('./../../public/scripts/board');
 
 exports.runSpecs = function(){
   describe('The Board', function(){
-
-    beforeAll(function(){
-      board.spots = [[],[],[]];
-    })
-
     afterEach(function(){
       board.spots = [[],[],[]];
     })
 
-    it('should initially represent tic tac toe board with empty 2d array', function(){
-      var gameBoard = board.spots;
+    it('should represent tic tac toe board with 2d array', function(){
+      var randomNumber = specHelper.getRandomIntBetween(0, 2);
 
-      expect(board.spots).toEqual([[], [], []]); 
+      expect(Array.isArray(board.spots[randomNumber])).toBeTruthy(); 
     });
 
     it('should reset 2d array to passed value with resetSpots', function(){
@@ -47,15 +42,92 @@ exports.runSpecs = function(){
 var specHelper = require('./spec-helper');
 var cpu = require('./../../public/scripts/cpu');
 var board = require('./../../public/scripts/board');
+var pieceValues = {
+  empty: -10,
+  human: 0,
+  cpu: 1
+}
 
 exports.runSpecs = function(){
   beforeEach(function(){
-    board.spots = [[],[],[]];
+    board.spots = [[pieceValues.empty, pieceValues.empty, pieceValues.empty],
+                   [pieceValues.empty, pieceValues.empty, pieceValues.empty],
+                   [pieceValues.empty, pieceValues.empty, pieceValues.empty]];
   })
 
   describe('The Cpu Brain', function(){
-    it('should wire up', function(){
-      expect(2 + 2).toEqual(4); 
+    it('should take corner spot on first move', function(){
+      var numberOfPreviousTurns = 0;
+      var spotSelected = cpu.determineSelection(board, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(0);
+      expect(spotSelected.column).toEqual(0); 
+    });
+
+    it('should take middle if available on 2nd move', function(){
+      var numberOfPreviousTurns = 1;
+      var rowOfPreviousSelection = 0
+      var columnOfPreviousSelection = 0;
+
+      board.selectionMade(pieceValues.human, rowOfPreviousSelection, columnOfPreviousSelection);
+      var spotSelected = cpu.determineSelection(board.spots, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(1);
+      expect(spotSelected.column).toEqual(1);
+    });
+
+    it('should take top left corner on 2nd move if middle is taken', function(){
+      var numberOfPreviousTurns = 1;
+      var rowOfPreviousSelection = 1
+      var columnOfPreviousSelection = 1;
+
+      board.selectionMade(pieceValues.human, rowOfPreviousSelection, columnOfPreviousSelection);
+      var spotSelected = cpu.determineSelection(board.spots, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(0);
+      expect(spotSelected.column).toEqual(0);      
+    });
+
+    it('should play winning move if available', function(){
+      var numberOfPreviousTurns = 4;
+
+      board.spots = [[pieceValues.cpu, pieceValues.human, pieceValues.human], 
+                     [pieceValues.empty, pieceValues.cpu, pieceValues.empty], 
+                     [pieceValues.empty, pieceValues.empty, pieceValues.empty]]
+
+
+      var spotSelected = cpu.determineSelection(board.spots, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(2);
+      expect(spotSelected.column).toEqual(2);  
+    });
+
+    it('should play winning move if both block and win are available', function(){
+      var numberOfPreviousTurns = 4;
+
+      board.spots = [[pieceValues.human, pieceValues.cpu, pieceValues.empty], 
+                     [pieceValues.human, pieceValues.cpu, pieceValues.empty], 
+                     [pieceValues.empty, pieceValues.empty, pieceValues.empty]]
+
+
+      var spotSelected = cpu.determineSelection(board.spots, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(2);
+      expect(spotSelected.column).toEqual(1);  
+    });
+
+    it('should play blocking move if needed and no win available', function(){
+      var numberOfPreviousTurns = 4;
+
+      board.spots = [[pieceValues.human, pieceValues.cpu, pieceValues.cpu], 
+                     [pieceValues.empty, pieceValues.human, pieceValues.empty], 
+                     [pieceValues.empty, pieceValues.empty, pieceValues.empty]]
+
+
+      var spotSelected = cpu.determineSelection(board.spots, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(2);
+      expect(spotSelected.column).toEqual(2);  
     });
   });
 }
--- jasmine/spec/cpu-spec.js
+++ jasmine/spec/cpu-spec.js
@@ -1,15 +1,92 @@
 var specHelper = require('./spec-helper');
 var cpu = require('./../../public/scripts/cpu');
 var board = require('./../../public/scripts/board');
+var pieceValues = {
+  empty: -10,
+  human: 0,
+  cpu: 1
+}
 
 exports.runSpecs = function(){
   beforeEach(function(){
-    board.spots = [[],[],[]];
+    board.spots = [[pieceValues.empty, pieceValues.empty, pieceValues.empty],
+                   [pieceValues.empty, pieceValues.empty, pieceValues.empty],
+                   [pieceValues.empty, pieceValues.empty, pieceValues.empty]];
   })
 
   describe('The Cpu Brain', function(){
-    it('should wire up', function(){
-      expect(2 + 2).toEqual(4); 
+    it('should take corner spot on first move', function(){
+      var numberOfPreviousTurns = 0;
+      var spotSelected = cpu.determineSelection(board, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(0);
+      expect(spotSelected.column).toEqual(0); 
+    });
+
+    it('should take middle if available on 2nd move', function(){
+      var numberOfPreviousTurns = 1;
+      var rowOfPreviousSelection = 0
+      var columnOfPreviousSelection = 0;
+
+      board.selectionMade(pieceValues.human, rowOfPreviousSelection, columnOfPreviousSelection);
+      var spotSelected = cpu.determineSelection(board.spots, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(1);
+      expect(spotSelected.column).toEqual(1);
+    });
+
+    it('should take top left corner on 2nd move if middle is taken', function(){
+      var numberOfPreviousTurns = 1;
+      var rowOfPreviousSelection = 1
+      var columnOfPreviousSelection = 1;
+
+      board.selectionMade(pieceValues.human, rowOfPreviousSelection, columnOfPreviousSelection);
+      var spotSelected = cpu.determineSelection(board.spots, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(0);
+      expect(spotSelected.column).toEqual(0);      
+    });
+
+    it('should play winning move if available', function(){
+      var numberOfPreviousTurns = 4;
+
+      board.spots = [[pieceValues.cpu, pieceValues.human, pieceValues.human], 
+                     [pieceValues.empty, pieceValues.cpu, pieceValues.empty], 
+                     [pieceValues.empty, pieceValues.empty, pieceValues.empty]]
+
+
+      var spotSelected = cpu.determineSelection(board.spots, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(2);
+      expect(spotSelected.column).toEqual(2);  
+    });
+
+    it('should play winning move if both block and win are available', function(){
+      var numberOfPreviousTurns = 4;
+
+      board.spots = [[pieceValues.human, pieceValues.cpu, pieceValues.empty], 
+                     [pieceValues.human, pieceValues.cpu, pieceValues.empty], 
+                     [pieceValues.empty, pieceValues.empty, pieceValues.empty]]
+
+
+      var spotSelected = cpu.determineSelection(board.spots, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(2);
+      expect(spotSelected.column).toEqual(1);  
+    });
+
+    it('should play blocking move if needed and no win available', function(){
+      var numberOfPreviousTurns = 4;
+
+      board.spots = [[pieceValues.human, pieceValues.cpu, pieceValues.cpu], 
+                     [pieceValues.empty, pieceValues.human, pieceValues.empty], 
+                     [pieceValues.empty, pieceValues.empty, pieceValues.empty]]
+
+
+      var spotSelected = cpu.determineSelection(board.spots, pieceValues, numberOfPreviousTurns);
+
+      expect(spotSelected.row).toEqual(2);
+      expect(spotSelected.column).toEqual(2);  
     });
   });
 }
